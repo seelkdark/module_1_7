52819bdc4521aa14a12f445749e5b1aa5efbc964
seelk <sedova08111984@gmail.com>
2024-10-20 23:48:55 +0300

    Add files via upload

---
+++ module_1_7.py
@@ -0,0 +1,9 @@
+immutable_var = 1,2,3,'конфета'
+print(type(immutable_var))
+print(immutable_var)
+#immutable_var[1] = 11 #не поддерживает назначение элементов
+#print(immutable_var)
+mutable_list = 1,2,4,'что'
+print(mutable_list)
+mutable_list = 1,2,4,'да'
+print(mutable_list)
