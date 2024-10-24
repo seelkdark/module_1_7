da4c0f06b5661a573d99d58518afd022d5b72c7f
seelk <sedova08111984@gmail.com>
2024-10-24 11:39:59 +0300

    Update module_1_7.py

--- module_1_7.py
+++ module_1_7.py
@@ -1,9 +1,8 @@
-immutable_var = 1,2,3,'конфета'
-print(type(immutable_var))
+immutable_var = ('чай', 15, 5.0, [9,8])
 print(immutable_var)
-#immutable_var[1] = 11 #не поддерживает назначение элементов
-#print(immutable_var)
-mutable_list = 1,2,4,'что'
-print(mutable_list)
-mutable_list = 1,2,4,'да'
+# immutable_var[1] = 3
+# TypeError: 'tuple' object does not support item assignment
+mutable_list = ['чай', 15, 5.0, [9,8]]
+mutable_list[0] = 'кофе'
+mutable_list[1] = 3
 print(mutable_list)
